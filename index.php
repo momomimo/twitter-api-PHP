de3975f8cd1e525c05f52588c39e34ceee7865c6
momomimo16 <mimow.aja@gmail.com>
2019-06-28 19:53:32 +0700

    twiiter-api-momo

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype HTML>
+<html lang="en">
+
+<head>
+	<title>Twitter Timeline @momomimo</title>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="robots" content="index,follow">
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+
+	<h1>Twitter API - Sutrimo 16.01.53.0183</h1>
+
+	<p>Twitter Api - Program Menampilkan Feed Timeline API Twitter Momomimo16 Dengan Bahasa Pemrograman PHP</p>
+
+	<?php include_once('_includes/twitter.php'); ?>
+
+	<footer>
+		<a href="https://github.com/momomimo/twitter-api-PHP">Source Code</a>
+	</footer>
+
+	<!-- jQuery library -->
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+	<!-- Optional: include //code.jquery.com/jquery-migrate-1.2.1.js if IE6/7/8 support is needed -->
+
+	<!-- Web Intents for Reply / Retweet / Favorite popup functionality (https://dev.twitter.com/docs/intents) -->
+	<script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
+	<!-- Custom Twitter functions -->
+	<script type="text/javascript" src="js/twitter.js"></script>
+</body>
+
+</html>
